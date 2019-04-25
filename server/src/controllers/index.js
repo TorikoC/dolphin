f964ddb86d5e72725eb05924b20a7c8887e7b77c
torikoc <breakthroughtgw@gmail.com>
2019-04-25 21:26:10 +0800

    chore: remove not use codes

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,13 +1,5 @@
 const router = require('express').Router();
-const cardCtrl = require('./card');
 const deckCtrl = require('./deck');
-
-router.get('/cards', cardCtrl.getCards);
-router.get('/random-cards', cardCtrl.getRandomCards);
-router.post('/cards', cardCtrl.createCard);
-router.put('/cards/:id', cardCtrl.updateCard);
-router.delete('/cards/:id', cardCtrl.removeCard);
-router.delete('/cards', cardCtrl.removeCards);
 
 router.get('/decks', deckCtrl.getDecks);
 router.post('/decks', deckCtrl.createDeck);
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,7 +1,5 @@
-const Card = require('./Card');
 const Deck = require('./Deck');
 
 module.exports = {
-  Card,
   Deck,
 };
